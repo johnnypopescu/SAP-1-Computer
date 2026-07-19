58f71c21a3a956380af972bd9b4b3d3c77d5e6ca
Johnny Popescu <johnnypopescu48@gmail.com>
2026-07-19 15:23:33 +0300

    Renamed some inputs/outputs and implameted RAM

--- ProgramCounter.sv
+++ ProgramCounter.sv
@@ -1,23 +1,23 @@
 module ProgramCounter(
 
-input       logic               count_enable,
-input       logic                        clk,
-input       logic                      rst_n,               
-input       logic              output_enable,
+input       logic                          cp,
+input       logic                       clk_n,
+input       logic                       rst_n,               
+input       logic                          ep, // enable pc pentru output  sa comunice cu busul
 output      logic   [7:0]              bus_w
 )
 
 logic       [3:0]       count;
 
-always_ff @(posedge clk or negedge rst_n) begin
+always_ff @(negedge clk_n or negedge rst_n) begin
     if(!rst_n)  begin
          count<= 0 ;
           end
-    else if (count_enable)begin
+    else if (cp)begin
         count <= count + 1;
     end
 end
 
-assign bus_w = output_enable? {4'b0000,count} : 8'hzz;
+assign bus_w = ep? {4'b0000,count} : 8'hzz;
 //buffer tristate
 endmodule    
--- RAM.sv
+++ RAM.sv
@@ -1,17 +1,33 @@
-module RAM(
-
-    logic       input            wr_en_n;
-    logic       input   [3:0]    address;
-    logic       output  [7:0]  bus_write;
-
+module Ram (
+    input  logic       ce_n,    // Chip enable, activ pe 0 (vine de la Controller)
+    input  logic [3:0] address, // Adresa (vine de la MAR)
+    output logic [7:0] bus_w    // Ieșirea spre magistrală
 );
-logic   [3:0]   ram     [15:0]
-initial begin 
-    case(address) 
-    0:ram[address] 
-    endcase
 
-end
+ 
+    logic [7:0] ram_array [0:15];
+
+    initial begin 
+        // ultimii biti sunt pentru instructiune , primii  4biti sunt pentru adresa de date
+        ram_array[0]  = 8'b0000_1001; // LDA 9
+        ram_array[1]  = 8'b0001_1010; // ADD 10
+        ram_array[2]  = 8'b0001_1011; // ADD 11
+        ram_array[3]  = 8'b0010_1100; // SUB 12
+        ram_array[4]  = 8'b1110_0000; // OUT
+        ram_array[5]  = 8'b1111_0000; // HLT
+        ram_array[6]  = 8'b0000_0000; 
+        ram_array[7]  = 8'b0000_0000; 
+        ram_array[8]  = 8'b0000_0000; 
+        ram_array[9]  = 8'b0001_0000; // Date (16)
+        ram_array[10] = 8'b0001_0100; // Date (20)
+        ram_array[11] = 8'b0001_1000; // Date (24)
+        ram_array[12] = 8'b0010_0000; // Date (32)
+        ram_array[13] = 8'b0000_0000; 
+        ram_array[14] = 8'b0000_0000; 
+        ram_array[15] = 8'b0000_0000; 
+    end
 
 
- endmodule
+    assign bus_w = (!ce_n) ? ram_array[address] : 8'hzz;
+
+endmodule
